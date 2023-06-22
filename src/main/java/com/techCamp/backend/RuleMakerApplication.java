a34a2acd65e59214f2406c1cff5740722db8b130
Luis Miguel Ossa Arias <luismiguelossaarias05@gmail.com>
2023-06-22 18:51:20 -0500

    feature(springboot): the base project has been defined

---
+++ src/main/java/com/techCamp/backend/RuleMakerApplication.java
@@ -0,0 +1,13 @@
+package com.techCamp.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuleMakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RuleMakerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techCamp/backend/RuleMakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techCamp.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuleMakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
