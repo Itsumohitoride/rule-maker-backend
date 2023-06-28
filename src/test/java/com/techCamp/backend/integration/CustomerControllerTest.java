6fad7fac7c6a312cc99117fe2f9bb350d53e6aa8
Gianni <70019070+G20-00@users.noreply.github.com>
2023-06-27 22:09:49 -0500

    Change in repository

---
+++ src/test/java/com/techCamp/backend/integration/CustomerControllerTest.java
@@ -0,0 +1,70 @@
+package com.techCamp.backend.integration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.techCamp.backend.dto.CreateUsersDTO;
+import com.techCamp.backend.dto.LoginDTO;
+import com.techCamp.backend.dto.TokenDTO;
+import com.techCamp.backend.integration.config.TestConfigurationData;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Import(TestConfigurationData.class)
+@ActiveProfiles(profiles = "test")
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class CustomerControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    public TokenDTO generateAdminToken() throws Exception{
+        var result = mockMvc.perform(MockMvcRequestBuilders.post("/login").content(
+                                objectMapper.writeValueAsString(new LoginDTO("jhonDoe@email.com", "password"))
+                        )
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn();
+        return objectMapper.readValue(result.getResponse().getContentAsString(), TokenDTO.class);
+    }
+    @Test
+    public void testCreateUser() throws Exception{
+        var newResult = mockMvc.perform(MockMvcRequestBuilders.post("/user").content(
+                                objectMapper.writeValueAsString(createCustomerDTO())
+                        )
+                        .header("Authorization","Bearer "+generateAdminToken().getToken())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        assertEquals(200, newResult.getResponse().getStatus());
+
+    }
+    private CreateUsersDTO createCustomerDTO() {
+        return CreateUsersDTO.builder()
+                .firstName("luis")
+                .lastName("andres")
+                .email("johndo@eemail.com")
+                .phoneNumber("+573258690188")
+                .role("admin")
+                .password("password").build();
+    }
+
+
+}
